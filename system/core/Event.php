f3126d1b5ffb7fb3ddf6c66e1170bf149598bf6f
Tamer Zorba <abo.al.tot@gmail.com>
2016-03-30 18:49:23 +0300

    Event : accept string to callback

--- system/core/Event.php
+++ system/core/Event.php
@@ -1,52 +1,67 @@
-<?php  if (!defined('VERSION'))exit('Direct access to this location is not permitted.');
+<?php
+ if (!defined('VERSION')) {
+     exit('Direct access to this location is not permitted.');
+ }
 /**
- * Purecis Event Module
+ * Purecis Event Module.
  *
  * This class Control Event Requests
  *
- * @package		codeHive
- * @subpackage	Module
  * @category	Libraries
+ *
  * @author		Tamer Zorba
+ *
  * @link		http://purecis.com/
  */
+class Event
+{
+    /**
+     * Variables.
+     *
+     * @var mixen
+     */
+    private static $events;
 
-class Event{
+    // --------------------------------------------------------------------
 
-	/**
-	 * Variables
-	 *
-	 * @var mixen
-	 * @access protected
-	 */
-	private static $events;
+    /**
+     * event addListener.
+     *
+     * @param	string
+     * @param	mixen
+     */
+    public static function addListener($event, $callback)
+    {
+        if (!is_callable($callback)) {
+            $callback = function () use ($callback) {
+                return $callback;
+            };
+        }
 
-	// --------------------------------------------------------------------
+        if (!isset(self::$events[$event])) {
+            self::$events[$event] = [];
+        }
+        array_push(self::$events[$event], $callback);
 
-	/**
-	 * event addListener
-	 *
-	 * @param	string
-	 * @param	mixen
-	 * @return	void
-	 */
-	public static function addListener($event,$callback){
-		if(!isset(self::$events[$event]))self::$events[$event] = [];
-		array_push(self::$events[$event], $callback);
-		return $callback;
-	}
+        return $callback;
+    }
 
+    // --------------------------------------------------------------------
 
-	// --------------------------------------------------------------------
+    /**
+     * event trigger.
+     *
+     * @return function
+     */
+    public static function trigger($event, $args = false)
+    {
+        $r = '';
+        if (isset(self::$events[$event])) {
+            foreach (self::$events[$event] as $c) {
+                $r .= call_user_func_array($c, array(&Controller::$scope, $args));
+            }
+        }
 
-	/**
-	 * event trigger
-	 *
-	 * @return	function
-	 */
-	public static function trigger($event, $args=false){
-		$r = "";
-		if(isset(self::$events[$event]))foreach(self::$events[$event] as $c)$r .= call_user_func_array($c, array(&Controller::$scope,$args));
-		return $r;
-	}
+        return $r;
+    }
 }
