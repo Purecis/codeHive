27298b763c23d35d863be688b0b5db005f9cc6c6
Tamer Zorba <abo.al.tot@gmail.com>
2016-03-30 18:50:50 +0300

    Core update
    codeHive Core : fix some notice and better code
    codeHive Core : use PSR-2

--- system/core/codeHive.php
+++ system/core/codeHive.php
@@ -1,124 +1,123 @@
 <?php
 /**
- * Purecis codeHive Class
+ * codeHive Core.
  *
- * bootstrap class
+ * Core class init codeHive Framework
  *
- * @package		codeHive
- * @subpackage	Core
- * @category	Libraries
- * @author		Purecis Dev Team
- * @copyright	Copyright (c) 2004 - 2015, Purecis, Inc.
- * @license		http://purecis.com/license
- * @link		http://purecis.com
- * @since		Version 2.0
- * @filesource
+ * @category    core
+ *
+ * @author      Tamer Zorba <abo.al.tot@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2016, PureCore International Solutions (http://purecis.com/)
+ * @license     http://opensource.org/licenses/MIT	MIT License
+ *
+ * @link       http://codehive.purecis.com/package/Benchmark
+ * @since      File available since Release 2.0.0
+ *
+ * @version    V: 2.1.0
  */
+class codeHive
+{
+    public static function start($start = array('app' => 'app'))
+    {
+        global $config;
 
-/**
- * auto load class when it called (Library classes only)
+        if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+            throw new Exception('The codeHive Framework v2.1 requires PHP version 5.3 or higher, (5.4) is Recomonded.');
+        }
+
+        define('VersionMajor', '2');
+        define('VersionMinor', '1');
+        define('VersionPatch', '00');
+        define('VersionCode', 'Beta');
+        define('VersionBuild', '3000');
+
+        define('VERSION', 'v.'.VersionMajor.'.'.VersionMinor.'.'.VersionPatch.' '.VersionCode.', Build '.VersionBuild);
+
+        $config = array();
+        $config['app'] = $start['app'] ?: 'app';
+        $config['assets'] = $start['assets'] ?: 'assets';
+        $config['system'] = $start['system'] ?: 'system';
+
+        $config = array_merge($config, self::config($config['app']));
+
+        if (!$config['settings']) {
+            $config['settings'] = array();
+        }
+
+        // defines
+        define('INDEX_FILE', isset($config['index']) ? $config['index'] : 'index.php');
+        define('SESSION_PERFIX', isset($config['session']) ? $config['session'] : '');
+
+        if (isset($config['license'])) {
+            if (isset($config['license']['hash'])) {
+                define('SECURITY_HASH', $config['license']['hash']);
+            }
+        }
+        if (isset($config['timezone'])) {
+            date_default_timezone_set($config['timezone']);
+        }
+
+        if (isset($config['ENVIRONMENT'])) {
+            switch ($config['ENVIRONMENT']) {
+                case 'development' :
+                    error_reporting(E_ALL);
+                break;
+
+                case 'debug' :
+                    error_reporting(E_ALL);
+                    Debug::__bootstrap();
+                break;
+
+                case 'production' :
+                    error_reporting(0);
+                break;
+
+                default:
+                    exit('The application environment is not set correctly.');
+            }
+        }
+
+        echo Module::__bootstrap();
+
+        require_once "{$start['app']}/app.php";
+
+        echo Module::__shutdown();
+
+        if (isset($config['ENVIRONMENT']) && $config['ENVIRONMENT'] == 'debug') {
+            Debug::__shutdown();
+        }
+    }
+
+    private static function config($app)
+    {
+        if (!file_exists("{$app}/config.php")) {
+            Install::initialize();
+            exit;
+        };
+
+        return require_once "{$app}/config.php";
+    }
+}
+
+/*
+ * auto load class when it called (Core Classes Only).
  */
-spl_autoload_register(function($class) {
-	global $config;
-	//if(strpos($class,"codeHive\Module\\") !== false){
-	//	$class = substr($class,strlen("codeHive\Module\\"));
-	//	Module::import($class);
-	//}else{
-		$class = str_replace("\\", "/", $class);
-		if(is_file("{$config['system']}/core/{$class}.php")){
-			require_once "{$class}.php";
-			if(is_callable(array($class,'__bootstrap')))call_user_func(array($class,'__bootstrap'));
-			if($config['ENVIRONMENT'] == 'debug')debug::message("Used System Cores",$class);
-		}else{
-			if($config['ENVIRONMENT'] == 'debug')debug::error("System Cores Faild",$class);
-		}
-	//}
+spl_autoload_register(function ($class) {
+    global $config;
+    $class = str_replace('\\', '/', $class);
+    if (is_file("{$config['system']}/core/{$class}.php")) {
+        require_once "{$class}.php";
+        if (is_callable(array($class, '__bootstrap'))) {
+            call_user_func(array($class, '__bootstrap'));
+        }
+        if (isset($config['ENVIRONMENT']) && $config['ENVIRONMENT'] == 'debug') {
+            debug::message('Used System Cores', $class);
+        }
+    } else {
+        if ($config['ENVIRONMENT'] == 'debug') {
+            debug::error('System Cores Faild', $class);
+        }
+    }
 });
-
-/**
- * Locale Alias __
- *
- * @access	public
- * @param	mixen 	str or array keys only
- * @return	mixen
- */
-function __($e,$a=array(),$space=false){
-	return Internationalization::translate($e,$a,$space);
-}
-
-/**
- * Class Start
- */
-class codeHive{//composer, artisan, Hive , Arti, Zorba, codeHive, iBuilder
-
-	public static function start($start = array('app'=>'app')){
-		global $config;
-
-		if (version_compare(PHP_VERSION, '5.3.0', '<')) {
-		  throw new Exception('The codeHive Framework v2 requires PHP version 5.3 or higher, (5.4) is Recomonded.');
-		}
-
-		define(VersionMajor,"2");
-		define(VersionMinor,"0");
-		define(VersionPatch,"00");
-		define(VersionCode,"Alpha");
-		define(VersionBuild,"2096");
-
-		define(VERSION,"v.".VersionMajor.".".VersionMinor.".".VersionPatch." ".VersionCode.", Build ".VersionBuild);
-
-		$config = array();
-		$config['app'] = $start['app']?:"app";
-		$config['assets'] = $start['assets']?:"assets";
-		$config['system'] = $start['system']?:"system";
-
-		$config = array_merge($config,self::config($config['app']));
-
-		if(!$config['settings'])$config['settings'] = array();
-
-		// defines
-		define(INDEX_FILE, $config['index']?:'index.php');
-		define(SESSION_PERFIX, $config['session']?:'');
-		if($config['license'])define(SECURITY_HASH, $config['license']['hash']);
-		if($config['timezone'])date_default_timezone_set($config['timezone']);
-
-		// env
-		if ($config['ENVIRONMENT']){
-			switch ($config['ENVIRONMENT']){
-				case 'development':
-					error_reporting(E_ALL);
-				break;
-
-				case 'debug':
-					error_reporting(E_ALL);
-					Debug::__bootstrap();
-				break;
-
-				case 'production':
-					error_reporting(0);
-				break;
-
-				default:
-					exit('The application environment is not set correctly.');
-			}
-		}
-
-		echo Module::__bootstrap();
-
-		require_once "{$start['app']}/app.php";
-
-		echo Module::__shutdown();
-
-		if($config['ENVIRONMENT'] == 'debug')Debug::__shutdown();
-	}
-
-	private static function config($app){
-		if(!file_exists("{$app}/config.php")){
-			Install::initialize();
-			exit;
-		};
-		return require_once "{$app}/config.php";
-	}
-}
-
 /* End of file codeHive.php */
 /* Location: ./system/core/codeHive.php */
