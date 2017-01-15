1b08ba93c6a2b77be1e9c9936937eeb9a95cbe0b
Tamer Zorba <abo.al.tot@gmail.com>
2017-01-15 14:51:28 +0200

    fix invokable with inject and invoke functions

--- system/Helpers/Invokable.php
+++ system/Helpers/Invokable.php
@@ -5,34 +5,55 @@ abstract class Invokable extends Injectable
 {
     public static $__namespace = "Invokable";
 
+    private static $__invokable = [];
+
+
+    // this will only inject the class to the system without calling any function
     public static function inject()
     {
         $args = func_get_args();
         $cls = new \stdClass;
 
-        foreach ($args as $argument) {
+        foreach ($args as $invokable) {
             // init class namespace
             $class = "\App\\";
             
-            // extracting module if exists
-
-            
-            if (Str::contains($argument, '@')) {
-                list($invokable, $module) = explode("@", $argument);
-                $class .= str_replace(".", "\\", $module) . "\\";
-            } else {
-                $invokable = $argument;
+            // when method exists then reparse path string (ClassName::Function@Container.Module)
+            Str::contains($invokable, '::') && list($invokable, $method) = explode("::", $invokable);
+                            
+            if (isset($method) && Str::contains($method, '@')) {
+                list($method, $module) = explode("@", $method);
             }
+
+            // extracting module if exists (ClassName@Container.Module)
+            else if (Str::contains($invokable, '@')) {
+                list($invokable, $module) = explode("@", $invokable);                
+            }
+
+            // replace Module.Name to psr-4 path Module\\Name for autoloader
+            isset($module) && $class .= str_replace(".", "\\", $module) . "\\";
             
             // complete injecting class namespace
             $class .= static::$__namespace . "\\" . $invokable;
 
+            // make (ClassName@Container.Module) to register it inside a invokable class
+            $path = $invokable . (isset($module) ? "@" . $module : "");
+
+            // check if class injected before or intialize it
+            if(!isset($__invokable[$path])){
+                $__invokable[$path] = (new $class);
+            }
+
             // call injected class
-            $cls->{$invokable} = (new $class);
+            $cls->{$invokable} = $__invokable[$path];
+
+            // set __invokable_method if method exists
+            isset($method) && $cls->{$invokable}->__invokable_method = $method;
 
             // Use DI -> check module __get as DI + Implicit Binding here
         }
         
+        // of only one just return it
         if (sizeof($args) == 1) {
             return $cls->{key($cls)};
         }
@@ -40,33 +61,23 @@ abstract class Invokable extends Injectable
         return $cls;
     }
 
-    // Explicit Binding here
+    // Explicit Binding here ( this will call function inside a class in pattern ) ClassName::Function@Container.Module
     public static function invoke()
     {
         // TODO : fix clusure callback
-
         $args = func_get_args();
         $cls = new \stdClass;
 
         foreach ($args as $argument) {
-            $class = "\App\\";
-            
-            list($invokable, $method) = explode("::", $argument);
-            if (Str::contains($method, '@')) {
-                list($method, $module) = explode("@", $method);
-                
-                $class .= str_replace(".", "\\", $module) . "\\";
-            }
-            self::inject($invokable . (isset($module) ? "@" . $module : ""));
+            $injectable = self::inject($argument);
+            $method = $injectable->__invokable_method;
 
-            $class .= static::$__namespace . "\\" . $invokable;
-
-            $params = self::extractParams($class, $method);
+            $params = self::extractParams($injectable, $method);
             $params = array_map(function ($e) {
                 return $e->type ? new $e->type : $e->value;
             }, $params);
 
-            $cls->{$method} = call_user_func_array(array((new $class), $method), $params);
+            $cls->{$method} = call_user_func_array(array($injectable, $method), $params);
         }
 
         if (sizeof($args) == 1) {
@@ -76,6 +87,8 @@ abstract class Invokable extends Injectable
         return $cls;
     }
 
+
+    // extract function arguments
     public static function extractParams($class, $method)
     {
         $arr = [];
