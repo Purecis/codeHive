1fc0f6b87c31c802ffb19e18e788b1abffcd094b
Tamer Zorba <abo.al.tot@gmail.com>
2017-03-04 22:36:13 +0200

    support view with modular and sub access

--- system/CoreServices/View.php
+++ system/CoreServices/View.php
@@ -5,12 +5,10 @@ class View
 {
     
     public static function inject($view, $scope=''){
-
-        // TODO : get view.name@model.name
-
-        $hive = new Scope('config.hive');
-        $path = $hive->app_path . "/view/" . $view . ".html";
-        $view = file_get_contents($path);
+        
+        $view = Loader::getDir('view/' . $view);
+        $view = str_replace(".", "/", $view) . ".html";
+        $view = file_get_contents($view);
         
         return Directive::trigger($view, $scope);
     }
