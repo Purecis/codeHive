3c86b1ad81c99ba0055a4e435162f96d7b322d0d
Tamer Zorba <abo.al.tot@gmail.com>
2017-03-07 15:30:55 +0200

    fix view parser if app name contain dots

--- system/CoreServices/View.php
+++ system/CoreServices/View.php
@@ -5,9 +5,9 @@ class View
 {
     
     public static function inject($view, $scope=''){
-        
-        $view = Loader::getDir('view/' . $view);
-        $view = str_replace(".", "/", $view) . ".html";
+
+        $view = str_replace(".", "/", $view);
+        $view = Loader::getDir('view/' . $view . ".html");
         $view = file_get_contents($view);
         
         return Directive::trigger($view, $scope);
