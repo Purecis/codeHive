78b642e82435256115784b1b1eb5cdc8b13f5f62
Tamer Zorba <abo.al.tot@gmail.com>
2017-03-10 19:04:52 +0200

    add http error code support to response

--- system/Http/Response.php
+++ system/Http/Response.php
@@ -5,10 +5,12 @@ namespace App\System;
 class Response
 {
     public $content = null;
-    public $type = "html";
-    public $headerSended = false;
+    private $type = "html";
+    private $headerSended = false;
+    private $code = 200;
 
-    public function __invoke(){ // called when the class name invoked
+    public function __invoke()
+    {
         $args = func_get_args();
         $this->body($args[0]);
         return $this;
@@ -16,30 +18,33 @@ class Response
 
     public function header($type = false)
     {
-        if($type){
+        if ($type) {
             $this->type = $type;
         }
 
-        if($this->headerSended){
+        if ($this->headerSended) {
             return $this;
         }
         
+        // set current http response code
+        http_response_code($this->code);
+
         // parse header by type
         if ($this->type == "json") {
             header('Content-Type: application/json');
-
         } elseif ($this->type == "plain") {
             header("Content-Type: text/plain");
-
-        } elseif ($this->type == "404") {
-            header("HTTP/1.0 404 Not Found");
-
         } else {
             header("Content-Type: text/html");
-
         }
         $this->headerSended = true;
 
+        return $this;
+    }
+    
+    public function code($code)
+    {
+        $this->code = $code;
         return $this;
     }
 
@@ -67,25 +72,23 @@ class Response
         return $this;
     }
 
-    public function view($view){
+    public function view($view)
+    {
         $this->content = View::inject($view);
         return $this;
     }
     
     public function spread()
     {
-        if(is_array($this->content) || is_object($this->content)){
+        if ((is_array($this->content) || is_object($this->content))) {
             $this->json();
+            $this->content = json_encode($this->content, JSON_NUMERIC_CHECK + JSON_PRETTY_PRINT);
         }
         
         $this->header();
-
-        if ($this->type == "json") {
-            $this->content = json_encode($this->content, JSON_NUMERIC_CHECK + JSON_PRETTY_PRINT);
-        }
-
         return print $this->content;
     }
+
     // download
     // file
     // redirect
