392d5a3ab88ee62ce777c0042c94762a2c6ba347
Tamer Zorba <abo.al.tot@gmail.com>
2017-04-02 17:05:34 +0300

    support global alias functions

--- system/CoreServices/_globals.php
+++ system/CoreServices/_globals.php
@@ -1,16 +1,21 @@
 <?php
+namespace {
 
-/*
- * set current working directory to the main index path
- */
-define('__CWD__', getcwd());
-chdir(dirname($_SERVER["SCRIPT_FILENAME"]));
-
-
-// register global functions here
+    /*
+    * set current working directory to the main index path
+    */
+    define('__CWD__', getcwd());
+    chdir(dirname($_SERVER["SCRIPT_FILENAME"]));
 
 
+    // alias for some functions
+    function random($length, $chars = false)
+    {
+        return App\System\Str::random($length, $chars);
+    }
 
-// function str_contains($haystack, $needles){
-//     App\System\Str::contains($haystack, $needles);
-// }
+    function str_contains($haystack, $needles)
+    {
+        return App\System\Str::contains($haystack, $needles);
+    }
+}
